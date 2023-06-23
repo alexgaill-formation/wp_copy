30ebdc108ef36f1a88c21f410730d5301e095dc9
Dylan <dylan77120@live.fr>
2023-06-23 15:17:19 +0200

    Copy-db: Copie la BDD dans un dossier Backup

--- scripts/copy_db.sh
+++ scripts/copy_db.sh
@@ -5,9 +5,12 @@
 GREEN=$(tput setaf 2)
 RESET=$(tput sgr0)
 
-# Demander à l'utilisateur de saisir les informations
+backup=$PWD
+parent_dir=$(dirname "$backup")
+
 read -p "${GREEN}Entrez le nom de votre BDD : ${RESET}" db_nom
 read -p "${GREEN}Entrez le nom de l'utilisateur : ${RESET}" db_user
 read -p "${GREEN}Entrez le mot de passe : ${RESET}" db_password
 read -p "${GREEN}Entrez le hostname : ${RESET}" db_host
-read -p "${GREEN}Quelle est le charset utilisé (ex: utf8mb4) : ${RESET}" db_charset
+
+/Applications/MAMP/Library/bin/mysqldump --no-tablespaces --host=$db_host --user=$db_user --password=$db_password $db_nom > $parent_dir/Backup/$db_nom.sql
