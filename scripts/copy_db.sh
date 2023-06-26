0be76fe312761d1498e1ec1fb974762b78d4490b
Dylan <dylan77120@live.fr>
2023-06-26 10:20:00 +0200

    Copy-db: Modifications PR

--- scripts/copy_db.sh
+++ scripts/copy_db.sh
@@ -5,6 +5,7 @@ function copy_db()
 {
     # Définir la couleur du texte en vert GREEN et remettre en couleur par défaut RESET
     GREEN=$(tput setaf 2)
+    RED=$(tput setaf 1)
     RESET=$(tput sgr0)
 
     # Chemin par défaut du répertoire
@@ -12,13 +13,18 @@ function copy_db()
     parent_dir=$(dirname "$backup")
 
     # Ajouts des prompts nécessaires pour la récupération de la BDD
-    read -p "${GREEN}Entrez le nom de votre BDD : ${RESET}" db_nom
-    read -p "${GREEN}Entrez le nom de l'utilisateur : ${RESET}" db_user
-    read -p "${GREEN}Entrez le mot de passe : ${RESET}" db_password
-    read -p "${GREEN}Entrez le hostname : ${RESET}" db_host
+    echo "${GREEN}Entrez le nom de votre BDD : ${RESET}";
+    read -p "> " db_nom
+    echo "${GREEN}Entrez le nom de l'utilisateur : ${RESET}";
+    read -p "> " db_user
+    echo "${GREEN}Entrez le mot de passe : ${RESET}";
+    read -p "> " db_password
+    echo "${GREEN}Entrez le hostname : ${RESET}";
+    read -p "> " db_host
 
     # Laisse le choix à l'utilisateur de choisir son chemin, si c'est vide le chemin pris est celui par défaut.
-    read -p "${GREEN}Entrez le chemin de votre BDD (laissez vide pour utiliser le répertoire de sauvegarde actuel) : ${RESET}" db_path
+    echo "${GREEN}Entrez le chemin de votre BDD (laissez vide pour utiliser le répertoire de sauvegarde actuel) : ${RESET}"
+    read -p "> " db_path
     db_path=${db_path:-$parent_dir/Backup}
 
     /Applications/MAMP/Library/bin/mysqldump --no-tablespaces --host=$db_host --user=$db_user --password=$db_password $db_nom > $db_path/$db_nom.sql
@@ -26,10 +32,10 @@ function copy_db()
     # Message de réussite ou d'echec
     if [[ $? -eq 0 ]]; then
         echo "$?"
-        echo "La sauvegarde a bien été exécutée ! :D"
+        echo "${GREEN}La sauvegarde a bien été exécutée ! :D"
     else 
         echo "$?"  
-        echo "La sauvegarde a échouée ! :'("
+        echo "${RED}La sauvegarde a échouée ! :'("
     fi      
 }
 copy_db
